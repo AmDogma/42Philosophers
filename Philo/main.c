85f314535ea14f31ef3c57c55d2490e6f13cc529
mdewayne <sawden@mail.ru>
2021-07-01 23:24:56 +0300

    begin

--- Philo/main.c
+++ Philo/main.c
@@ -1,15 +1,39 @@
-//
-//  main.c
-//  Philo
-//
-//  Created by Malena Dewayne on 7/1/21.
-//  Copyright © 2021 Malena Dewayne. All rights reserved.
-//
+#include "philo.h"
 
-#include <stdio.h>
+void	ft_error(char *message)
+{
+	int	i;
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
-	printf("Hello, World!\n");
+	i = 0;
+	while (message + i && message[i] != '\0')
+		write(1, (message + i++), 1);
+	write(1, "\n", 1);
+	exit (-1);
+}
+
+int ft_num(char *num)
+{
+	int i;
+
+	i = 0;
+	while (*num >= 0 && *num <= 9)
+		i = i * 10 + i;
+	if (*num)
+		i = -1;
+	return(i);
+}
+
+int main(int argc, char **argv)
+{
+	t_tab par;
+
+	if (argc != 5)
+		ft_error("Error: Wrong number of arguments");
+	par.phil = ft_num(argv[1]);
+	par.die = ft_num(argv[2]);
+	par.eat = ft_num(argv[3]);
+	par.sleep = ft_num(argv[4]);
+	if (par.phil < 1 || par.sleep < 1 || par.eat < 1 || par.die < 1)
+		ft_error("Error: Wrong arguments");
 	return 0;
 }
---
+++ Philo/philo.h
@@ -0,0 +1,14 @@
+#include <stdio.h> // delete
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_tab
+{
+	int phil;
+	int die;
+	int eat;
+	int sleep;
+}	t_tab;
+
+void	ft_error(char *message);
