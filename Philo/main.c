a04f0ffb35c53e972b84924ecc7313ec153c9531
mdewayne <sawden@mail.ru>
2021-07-02 23:58:03 +0300

    day two

--- Philo/main.c
+++ Philo/main.c
@@ -16,24 +16,49 @@ int ft_num(char *num)
 	int i;
 
 	i = 0;
-	while (*num >= 0 && *num <= 9)
-		i = i * 10 + i;
+	while (*num >= '0' && *num <= '9')
+	{
+		i = i * 10 + *num;
+		num++;
+	}
 	if (*num)
 		i = -1;
 	return(i);
 }
 
-int main(int argc, char **argv)
+void parse_param(t_tab *par, char **argv)
+{
+	par->phil = ft_num(argv[1]);
+	par->die = ft_num(argv[2]);
+	par->eat = ft_num(argv[3]);
+	par->sleep = ft_num(argv[4]);
+	if (par->phil < 1 || par->sleep < 1 || par->eat < 1 || par->die < 1)
+		ft_error("Error: Wrong arguments");
+}
+
+int philo(int argc, char **argv) // main
 {
 	t_tab par;
 
 	if (argc != 5)
 		ft_error("Error: Wrong number of arguments");
-	par.phil = ft_num(argv[1]);
-	par.die = ft_num(argv[2]);
-	par.eat = ft_num(argv[3]);
-	par.sleep = ft_num(argv[4]);
-	if (par.phil < 1 || par.sleep < 1 || par.eat < 1 || par.die < 1)
-		ft_error("Error: Wrong arguments");
+	parse_param(&par, argv);
+	start_phil(&par);
+	return 0;
+}
+
+int main() // del
+{
+	unsigned long real_time;
+
+	real_time = msec_c();
+	printf("%ld\n", real_time);
+	usleep(200*1000);
+
+	real_time = msec_c() - real_time;
+	printf("%ld\n", real_time);
+
+	char *argv[] = {"phil", "5", "800", "200", "200"};
+	philo(5, argv);
 	return 0;
 }
